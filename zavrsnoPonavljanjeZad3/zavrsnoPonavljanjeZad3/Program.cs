86f54834ff8e26dd1b11b4b392ec8203e416df6b
Neven <58472164+neven14@users.noreply.github.com>
2021-05-31 09:35:57 +0200

    Add files via upload

---
+++ zavrsnoPonavljanjeZad3/zavrsnoPonavljanjeZad3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsnoPonavljanjeZad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj;
+            Console.Write("Upisi broj koji zelis podijeliti: ");
+            broj = Convert.ToInt32(Console.ReadLine());
+            if( broj % 4 == 0 && broj % 6 != 0)
+            {
+                Console.WriteLine("Broje je dijeljiv s 4");
+            }
+            if (broj % 6 == 0 && broj % 4 != 0)
+            {
+                Console.WriteLine("Broje je dijeljiv s 6");
+            }
+            if (broj % 6 == 0 && broj % 4 == 0)
+            {
+                Console.WriteLine("Broje je dijeljiv s 6 i 4");
+            }
+            if (broj % 6 != 0 && broj % 4 != 0)
+            {
+                Console.WriteLine("Broje nije dijeljiv s 6 i 4");
+            }
+            Console.ReadKey();
+        }
+    }
+}
